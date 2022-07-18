198509eff17ecc5d399bcb4db26ba56a6e0afed5
JeonJaeHyeong <wogud405@kaist.ac.kr>
2022-07-18 11:17:56 +0900

    NCF add

--- load_rating_data.py
+++ load_rating_data.py
@@ -1,5 +1,23 @@
 import numpy as np
 import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+from sklearn.model_selection import train_test_split
+import copy
+import torch
+
+
+class RatingDataset(Dataset):
+    def __init__(self, user_tensor, item_tensor, values):
+        self.user_tensor = user_tensor
+        self.item_tensor = item_tensor
+        self.values = values
+        
+    def __len__(self):
+        return self.user_tensor.size(0)
+    
+    def __getitem__(self, index):
+        return self.user_tensor[index], self.item_tensor[index], self.values[index]
+
 
 
 # load ml-100k rating data
@@ -36,3 +54,73 @@ def load_rating_1m():
     rating = rating[['user_id', 'item_id', 'rating', 'timestamp']]
     
     return rating
+
+class information():
+
+    def __init__(self, args):
+        mask = (args.df.user_id <= 50)
+        self.df = args.df.loc[mask, :]     # memory issue
+        self.n_users = args.n_users = len(self.df.user_id.unique())
+        self.n_items = args.n_items = len(self.df.item_id.unique())
+        self.train, self.test = self.split_df()
+        
+        self.train_neg_df = self.get_neg_items(self.train, args.train_n_neg)
+        self.train_dataset = self.get_dataset(self.train, self.train_neg_df, "implicit_neg")
+        self.train_dataloader = DataLoader(self.train_dataset, batch_size = args.batch_size, shuffle = True)
+
+        self.test_neg_df = self.get_neg_items(self.test, args.test_n_neg)
+        self.test_dataset = self.get_dataset(self.test, self.test_neg_df, "neg_samples")
+        self.test_dataloader = DataLoader(self.test_dataset, batch_size = args.batch_size, shuffle = True)
+        
+    def split_df(self):
+        
+        ratio = 0.8
+        train, test = train_test_split(self.df, test_size=1-ratio)
+        train.reset_index(drop=True, inplace=True)
+        test.reset_index(drop=True, inplace=True)        
+        return train, test
+
+    def get_neg_items(self, dataframe, n_neg_samples):
+        pos_item_dic = {}
+        neg_item_dic = {}
+        total_item = np.arange(self.n_items)
+        neg_item = []
+        for row in dataframe.itertuples():
+            if row.user_id not in pos_item_dic.keys():
+                mask = (dataframe.user_id == row.user_id)
+                pos_item_dic[row.user_id] = np.array(dataframe.loc[mask, :].item_id)
+                neg_item_dic[row.user_id] = np.setdiff1d(total_item, pos_item_dic[row.user_id])
+            neg = np.random.choice(neg_item_dic[row.user_id], n_neg_samples)
+            neg_item.append([neg])
+        neg_df = pd.DataFrame(neg_item)
+        neg_df.columns = ["negative"]       
+        return pd.concat([dataframe.user_id, neg_df], axis=1)
+
+
+    def get_dataset(self, dataframe, neg_df,  dataset_type):
+        
+        merged_df = pd.merge(dataframe, neg_df, on= "user_id")
+        if dataset_type == "neg_samples":
+            train_dataset = RatingDataset(user_tensor = torch.LongTensor(merged_df.user_id),
+                                item_tensor = torch.LongTensor(merged_df.item_id),
+                                values = torch.LongTensor(merged_df.negative))
+        
+        elif dataset_type == "implicit_neg":
+            users, items, implicit = [], [], []
+            
+            for row in merged_df.itertuples():
+                users.append(int(row.user_id))
+                items.append(int(row.item_id))
+                implicit.append(float(1))
+                
+                for neg in row.negative:
+                    users.append(int(row.user_id))
+                    items.append(int(neg))
+                    implicit.append(float(0))
+            
+            
+            train_dataset = RatingDataset(user_tensor = torch.LongTensor(users),
+                                item_tensor = torch.LongTensor(items),
+                                values = torch.FloatTensor(implicit))            
+        return train_dataset
+            
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 def RMSE(R, pred):
     
@@ -13,3 +14,26 @@ def RMSE(R, pred):
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+
+def hit_ndcg(model, test_data):
+    
+    hit = 0
+    ndcg = 0
+    
+    for i in range(len(test_data)):
+        test_pred = torch.FloatTensor(model(test_data[i][0], test_data[i][1])).view(-1, 1)
+        neg_pred = torch.FloatTensor(model(test_data[i][0].expand(99), test_data[i][2]))
+        concat = torch.cat([test_pred, neg_pred]).view(-1)
+            
+        _, indices = torch.topk(concat, 10)
+        indices = indices.numpy().tolist()
+        if 0 in indices:
+            hit += 1
+            index = indices.index(0)
+            ndcg += np.reciprocal(np.log2(index+2))
+            
+    hit_ratio = hit / len(test_data)
+    ndcg = ndcg / len(test_data)
+    
+    return hit_ratio, ndcg
